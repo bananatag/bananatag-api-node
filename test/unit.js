2dbaf3daf82564b119f4d9cfb00c501ef07d3bb5
Isaac Oslund <ijoslund@gmail.com>
2015-03-25 16:52:58 -0700

    Updated test

--- test/unit.js
+++ test/unit.js
@@ -10,7 +10,7 @@ var credentails = {
 describe('Testing basic API requests:', function () {
 
     var btag = new BTagAPI(credentails.authID, credentails.key);
-    var params = {};
+    var params = {start: '2013-01-01', end: '2014-03-30'};
 
     it('Validate credentials', function (done) {
         credentails.authID.should.not.equal('');
@@ -20,7 +20,6 @@ describe('Testing basic API requests:', function () {
     });
 
     it('Get tags within date range', function (done) {
-        var btag = new BTagAPI(credentails.authID, credentails.key);
         btag.request('tags', params, function (err, data) {
             should.not.exist(err);
 
